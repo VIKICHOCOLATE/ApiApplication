07392318f429134d970a5245defbe04d92330bd3
vsuponenka <viktoriasuponenko@gmail.com>
2023-11-01 14:31:21 +0100

    Added a project

---
+++ ApiApplication/Controllers/ShowtimesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiApplication.Controllers
+{
+    public class ShowtimesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
